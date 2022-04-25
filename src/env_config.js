014c01761e409883d3d44add8dc6f7f5c245f2de
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2022-04-26 01:58:29 +0700

    m

--- src/env_config.js
+++ src/env_config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  API_URL: prod ? 'https://bookapiv1.herokuapp.com/' : 'http://localhost:5000/api',
+  API_URL: prod ? 'https://bookapiv1.herokuapp.com/api' : 'http://localhost:5000/api',
 };
