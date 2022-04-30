caa1922a6c52c30420b1ff006a1fc8263586cec8
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2022-04-30 20:15:30 +0700

    update dashboard

--- src/components/Common/PieChart.js
+++ src/components/Common/PieChart.js
@@ -24,7 +24,7 @@ function PieChart(props) {
   },
   yAxis: {
       title: {
-          text: 'Total percent market share'
+          text: 'Số lượng'
       }
 
   },
@@ -36,59 +36,15 @@ function PieChart(props) {
           borderWidth: 0,
           dataLabels: {
               enabled: true,
-              format: '{point.y:.1f}%'
+              format: '{point.y:.1f}'
           }
       }
   },
 
   tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+      // headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b><br/>'
   },
-
-  series: [
-      {
-          name: "Browsers",
-          colorByPoint: true,
-          data: [
-              {
-                  name: "Chrome",
-                  y: 62.74,
-                  drilldown: "Chrome"
-              },
-              {
-                  name: "Firefox",
-                  y: 10.57,
-                  drilldown: "Firefox"
-              },
-              {
-                  name: "Internet Explorer",
-                  y: 7.23,
-                  drilldown: "Internet Explorer"
-              },
-              {
-                  name: "Safari",
-                  y: 5.58,
-                  drilldown: "Safari"
-              },
-              {
-                  name: "Edge",
-                  y: 4.02,
-                  drilldown: "Edge"
-              },
-              {
-                  name: "Opera",
-                  y: 1.92,
-                  drilldown: "Opera"
-              },
-              {
-                  name: "Other",
-                  y: 7.62,
-                  drilldown: null
-              }
-          ]
-      }
-  ],
   series:props.series
   
   };
