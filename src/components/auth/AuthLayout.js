2b23c8cbb1b106b9340a45081fa5ed7e31e62b56
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2022-05-14 16:56:03 +0700

    update login

--- src/components/auth/AuthLayout.js
+++ src/components/auth/AuthLayout.js
@@ -18,10 +18,10 @@ function AuthLayout(props) {
       <div className="dark-overlay">
         <div className="auth-inner">
           <div style={{ backgroundColor: 'white', borderRadius: 8, padding: 16, display: 'flex', flexDirection: 'column' }}>
-            <h3>Đăng nhập để mua sách ngay trong ngày hôm nay</h3>
-            <div className="mt-16" style={{ width: 400 }}>
+            <h2 style={{color:'#c46c49'}}>Đăng nhập để mua sách ngay trong ngày hôm nay</h2>
+            <div className="mt-16" style={{ width: '100%' }}>
               {(status && <Login {...props} />) || <Register {...props} onButtonLogin={onButtonLogin} statusBtn={status} />}
-              {(status&& <div className="mt-16" >Bạn chưa có tài khoản? <span onClick={()=>onButtonRegister()} className="fw-500 cursor-pointer" style={{color:'blue'}}>Đăng ký ngay!</span></div>||<div className="mt-16" >Bạn đã có tài khoản? <span onClick={()=>onButtonLogin()} className="fw-500 cursor-pointer" style={{color:'blue'}}>Đăng nhập ngay!</span></div>)}
+              {(status&& <div className="mt-16" >Bạn chưa có tài khoản? <span onClick={()=>onButtonRegister()} className="fw-500 cursor-pointer" style={{color:'#c46c49'}}>Đăng ký ngay!</span></div>||<div className="mt-16" >Bạn đã có tài khoản? <span onClick={()=>onButtonLogin()} className="fw-500 cursor-pointer" style={{color:'#c46c49'}}>Đăng nhập ngay!</span></div>)}
             </div>
           </div>
         </div>
